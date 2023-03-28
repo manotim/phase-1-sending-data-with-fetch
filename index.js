2ab8509f2c16ffcbd167bef75c4d772b6a0402f4
manoti <manotimike@gmail.com>
2023-03-28 12:39:57 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+        }),
+      })
+      .then((res) => {
+        if(!res.ok) {
+          throw new Error('network not working')
+        }
+        return res.json()
+      })
+      .then((data) => {
+        let newId = data.id
+        let idText = document.createTextNode(newId)
+        document.body.appendChild(idText)
+      })
+      .catch((error) => {
+        // console.error('error occured', error)
+        let textError = document.createTextNode(error.message)
+        document.body.appendChild(textError)
+      })
+      
+}
